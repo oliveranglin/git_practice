eebd04f0bbd7fd64bda31b30f4f508b1b6646748
Oliver Anglin <oliveranglin@hotmail.co.uk>
2021-08-05 12:06:41 +0100

    .js file created, but desired output in console

--- Horror Scope.js
+++ Horror Scope.js
@@ -1,3 +1,22 @@
-console.log('hello');
-let test = Math.floor(Math.random() * 8);
-console.log(test);
+const actions = ['malled', 'haunted', 'bitten', 'ripped', 'intimidated'];
+
+const effects = ['to death', 'to insanity', 'to an inch of your life', 'greviously', 'mildly'];
+
+const beasts = ['ghost', 'stalker', 'demon', 'zombie', 'vampire'];
+
+
+let randomSelect1 = Math.floor(Math.random() * 5)
+let randomSelect2 = Math.floor(Math.random() * 5)
+let randomSelect3 = Math.floor(Math.random() * 5)
+
+
+const randSentence = (array1, array2, array3) => {
+    let i = randomSelect1;
+    let j = randomSelect2;
+    let k = randomSelect3;
+
+    return `You will be ${array1[i]} ${array2[j]} by a ${array3[k]}!`
+};
+
+
+console.log(randSentence(actions, effects, beasts));
